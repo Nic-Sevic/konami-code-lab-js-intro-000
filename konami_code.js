db543fc9a31a70829fa4eb1c73e4cbd862727334
Nic Sevic <nasevic@gmail.com>
2019-03-31 21:03:48 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,7 +12,7 @@ const codes = [
 ];
 
 function init() {
-  const body = document.querySelector('body') body.addEventListener('keydown', function(event))
+  const body = document.querySelector('body') 
   body.addEventListener('keydown',function(e){onKeyDown()})
 
   let index = 0
