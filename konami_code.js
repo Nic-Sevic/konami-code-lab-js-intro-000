5e5138e5e5887feab3a2624d2023ffa7b2959515
Nic Sevic <nasevic@gmail.com>
2019-03-31 21:35:34 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -10,21 +10,24 @@ const codes = [
   "b",
   "a"
 ];
+
 let index = 0
+
 function init() {
   const body = document.querySelector('body')
-  body.addEventListener('keydown', codeTest(e){
+  body.addEventListener('keydown', codeTest())}
+
+function codeTest(e) {
   let key = e.key
-    if (key === codes[index]) {
-      index++
-    }
-    if (index === codes.length) {
-      alert('Game Unlocked')
-      index = 0
-    }
-    else {
-      index = 0
-      continue
-    }
-  })
+  if (key === codes[index]) {
+    index++
+  }
+  if (index === codes.length) {
+    alert('Game Unlocked')
+    index = 0
+  }
+  else {
+    index = 0
+    continue
+  }
 }
