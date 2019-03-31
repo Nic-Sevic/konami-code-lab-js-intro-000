199bcfe24bf41c4afdfb32214efbb9e765c6e042
Nic Sevic <nasevic@gmail.com>
2019-03-31 21:48:24 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -25,11 +25,12 @@ function codeTest(e) {
   if (key === codes[index]) {
     index++
   }
+  else {
+    index = 0
+  }
   if (index === codes.length) {
     alert('Game Unlocked')
     index = 0
   }
-  else {
-    index = 0
-  }
+  
 }
